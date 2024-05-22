41ce9f1e901cbc5966d0588ddb3ebfd61ce5dc87
Erfan Momeni <erfammioi@gmail.com>
2024-05-22 12:56:42 +0330

    fix: add suitable annotations

--- crawl/alibabascraper.py
+++ crawl/alibabascraper.py
@@ -2,26 +2,30 @@ import os
 import requests
 import configparser
 import json
-from crawl.models import flight, hotel
+from crawl.models.flight import flight
+from crawl.models.hotel import hotel
 
 alibaba_flight_url = "https://ws.alibaba.ir/api/v1/flights"
 alibaba_hotel_url = "https://ws.alibaba.ir/api/v1/hotel"
 
 
 def save_to_file(data, file_path=''):
-    with open(file_path, 'a',encoding='utf-8') as file:
+    with open(file_path, 'a', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
 
 
 class AlibabaScraper:
-    def __init__(self, scrap=[]):
+    def __init__(self, scrap=None):
+        if scrap is None:
+            scrap = []
         if 'flight' in scrap:
             self.flight_scraper()
 
         if 'hotel' in scrap:
             self.hotel_scraper()
 
-    def flight_scraper(self):
+    @staticmethod
+    def flight_scraper():
         config = configparser.ConfigParser()
         script_dir = os.path.dirname(os.path.abspath(__file__))
         config_path = os.path.join(script_dir, 'config.ini')
@@ -62,13 +66,13 @@ class AlibabaScraper:
                                     departing_flights = response['result']['departing']
                                     returning_flights = response['result']['returning']
                                     for df in departing_flights:
-                                        f = flight.flight(df['originName'], df['destinationName'], df['leaveDateTime'],
+                                        f = flight(df['originName'], df['destinationName'], df['leaveDateTime'],
                                                           df['arrivalDateTime'],
                                                           df['priceAdult'], df['airlineName'])
                                         flights.append(f)
 
                                     for rf in returning_flights:
-                                        f = flight.flight(rf['originName'], rf['destinationName'], rf['leaveDateTime'],
+                                        f = flight(rf['originName'], rf['destinationName'], rf['leaveDateTime'],
                                                           rf['arrivalDateTime'],
                                                           rf['priceAdult'], rf['airlineName'])
                                         flights.append(f)
@@ -77,12 +81,14 @@ class AlibabaScraper:
 
         data = [f.to_dict() for f in flights]
         save_to_file(data=data, file_path=os.path.join(script_dir, 'flights.json'))
+        return
 
-    def hotel_scraper(self):
+    @staticmethod
+    def hotel_scraper():
         config = configparser.ConfigParser()
         script_dir = os.path.dirname(os.path.abspath(__file__))
         config_path = os.path.join(script_dir, 'config.ini')
-        config.read(config_path,encoding='utf-8')
+        config.read(config_path, encoding='utf-8')
 
         dates = config['dates']['list'].split(',')
         hotel_cities = config['cities']['hotel_list'].split(',')
@@ -141,7 +147,7 @@ class AlibabaScraper:
                                                             json=data).json()
                                 hs = response["result"]["result"]
                                 for h in hs:
-                                    ht = hotel.hotel(
+                                    ht = hotel(
                                         h['name']['fa'],
                                         h['country']['name']['fa'],
                                         h['city']['name']['fa'],
