ab784222f204cab333d9a1be1d322d4c8212326e
Erfan Momeni <erfammioi@gmail.com>
2024-05-25 17:26:42 +0330

    feat: add agent and define state

---
+++ agent.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from langchain_community.llms import Ollama
+from state import State
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnableConfig
+
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            passenger_id = config.get("passenger_id", None)
+            state = {**state, "user_info": passenger_id}
+            result = self.runnable.invoke(state)
+            # If the LLM happens to return an empty response, we will re-prompt it
+            # for an actual response.
+            if not result.tool_calls and (
+                    not result.content
+                    or isinstance(result.content, list)
+                    and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+
+
+llm = Ollama(model="llama3")
+
+primary_assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Alibaba Airlines. "
+            " Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n\n{user_info}\n"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now())
+
+part_1_tools = []
+part_1_assistant_runnable = primary_assistant_prompt | llm.bind_tools(part_1_tools)
